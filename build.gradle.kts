0a873042fc74b5ceddbd34bea9bd69552004b167
Shayan Mouktar <119674649+Shayan105@users.noreply.github.com>
2024-10-02 16:13:50 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "EndurAi_TestCI")
+    property("sonar.organization", "enduraimakesmybodymoves1919")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
